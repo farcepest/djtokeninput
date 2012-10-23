0417ae4b49ecc47920a84a59d279cddee3cfa2ed
Andy Dustman <farcepest@gmail.com>
2012-10-23 15:10:54 -0400

    Example update to use querysets on the field.

--- example/app/forms.py
+++ example/app/forms.py
@@ -8,4 +8,6 @@ from app import models
 class ExampleForm(forms.Form):
   title = forms.CharField()
   desc = forms.CharField(widget=forms.Textarea)
-  tags = TokenField(models.Tag, required=False, search_view="search_tags")
+  tags = TokenField(models.Tag.objects.all(), required=False, search_view="search_tags")
+  show = TokenField(models.Tag.objects.all(), required=False, search_url="http://shell.loopj.com/tokeninput/tvshows.php")
+  # the definition for show works by virtue of not saving the form
