92ac7758999ccefbd1404af3cdbed1292ca99db8
Andy Dustman <farcepest@gmail.com>
2012-10-23 15:06:45 -0400

    Use a queryset instead of a model like every other field.

--- lib/djtokeninput/fields.py
+++ lib/djtokeninput/fields.py
@@ -12,16 +12,14 @@ class TokenField(forms.ModelMultipleChoiceField):
   def _class_name(value):
     return value.replace(" ", "-")
 
-  def __init__(self, model, *args, **kwargs):
+  def __init__(self, queryset, *args, **kwargs):
     widget_attrs = { }
 
     for name in self.kwargs_for_widget:
       if name in kwargs:
         widget_attrs[name] = kwargs.pop(name)
 
-    super(TokenField, self).__init__(model.objects.all(), *args, **kwargs)
-    self.widget.class_name = self._class_name(model._meta.verbose_name_plural)
-    self.widget.model = model
+    super(TokenField, self).__init__(queryset, *args, **kwargs)
 
     for name in widget_attrs:
       setattr(self.widget, name, widget_attrs[name])
