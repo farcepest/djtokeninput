0c9795bf3f851bef2333731aeae0236ec113f952
Andy Dustman <farcepest@gmail.com>
2012-10-23 17:54:18 -0400

    Enforce queryset return limits.

--- lib/djtokeninput/views.py
+++ lib/djtokeninput/views.py
@@ -10,17 +10,47 @@ class JSONSearchView(MultipleObjectMixin, View):
 
     query_parameter = "q"
     min_query_length = 1
+    queryset_limit_factor = 10
+    queryset_limit_max = 50
     response_class = HttpResponse
     allow_empty = True
 
-    def filter_queryset(self, qs, query):
-        """This method should take a queryset in qs, and return a queryset, filtered in some way by
-        query."""
-        raise NotImplemented, "This method must be overridden"
+    def get_max_results(self):
+        """
+        Computes the upper bound for the size of the result set.
+        :return:
+            Returns the maximum number of results allowed for this particular query.
+            This is computed as:
+                (length of query string)*(minimum query length)*(queryset limit factor)
+            i.e. longer query strings allow for a longer result set.
+            queryset_limit_max is the absolute upper limit, if set to a non-False value.
+            Note that an empty query string (length 0) generates a limit of 0.
+        :rtype:
+            int
+        """
+        query_length = len(self.query.strip())
+        max_results = self.min_query_length*query_length*self.queryset_limit_factor
+        if self.queryset_limit_max:
+            return max(max_results, self.queryset_limit_max)
+        else:
+            return max_results
+
+    def limit_queryset(self, qs):
+        M = self.get_max_results()
+        if M:
+            return qs[:]
+        else:
+            return qs
+
+    def filter_queryset(self, qs):
+        return qs.filter(name__istartswith=self.query)
 
     def get(self, request, *args, **kwargs):
         self.query = request.GET.get(self.query_parameter, "")
-        self.object_list = self.filter_queryset(self.get_queryset(), self.query)
+        object_list = self.get_queryset()
+        object_list = self.filter_queryset(object_list)
+        object_list = self.limit_queryset(object_list)
+        self.object_list = object_list
         allow_empty = self.get_allow_empty()
         if not allow_empty and len(self.object_list) == 0:
             raise Http404(_(u"Empty list and '%(class_name)s.allow_empty' is False.")
