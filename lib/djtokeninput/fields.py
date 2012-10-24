22908b1243bd8e1b15d4c49d5b009366de07caa4
Andy Dustman <farcepest@gmail.com>
2012-10-24 13:48:23 -0400

    PEP 8, sucka

--- lib/djtokeninput/fields.py
+++ lib/djtokeninput/fields.py
@@ -5,21 +5,22 @@ from djtokeninput.widgets import TokenWidget
 
 
 class TokenField(forms.ModelMultipleChoiceField):
-  kwargs_for_widget = ("search_view","search_url","render_value")
-  widget = TokenWidget
 
-  @staticmethod
-  def _class_name(value):
-    return value.replace(" ", "-")
+    kwargs_for_widget = ("search_view", "search_url", "render_value")
+    widget = TokenWidget
 
-  def __init__(self, queryset, *args, **kwargs):
-    widget_attrs = { }
+    @staticmethod
+    def _class_name(value):
+        return value.replace(" ", "-")
 
-    for name in self.kwargs_for_widget:
-      if name in kwargs:
-        widget_attrs[name] = kwargs.pop(name)
+    def __init__(self, queryset, *args, **kwargs):
+        widget_attrs = {}
 
-    super(TokenField, self).__init__(queryset, *args, **kwargs)
+        for name in self.kwargs_for_widget:
+            if name in kwargs:
+                widget_attrs[name] = kwargs.pop(name)
 
-    for name in widget_attrs:
-      setattr(self.widget, name, widget_attrs[name])
+        super(TokenField, self).__init__(queryset, *args, **kwargs)
+
+        for name in widget_attrs:
+            setattr(self.widget, name, widget_attrs[name])
--- lib/djtokeninput/widgets.py
+++ lib/djtokeninput/widgets.py
@@ -8,74 +8,75 @@ from django.core.urlresolvers import reverse
 
 
 class TokenWidget(forms.TextInput):
-  class Media:
-    css = {
-      "all": ("css/token-input.css",)
-    }
 
-    js = (
-      "js/jquery.tokeninput.js",
-      "js/djtokeninput.js"
-    )
+    class Media:
+        css = {
+            "all": ("css/token-input.css",)
+        }
 
-  search_url = None
-  search_view = None
+        js = (
+            "js/jquery.tokeninput.js",
+            "js/djtokeninput.js"
+        )
 
-  def __init__(self, attrs=None, **kwargs):
-    super(TokenWidget, self).__init__(attrs)
-    self.settings = self._normalize(kwargs)
+    search_url = None
+    search_view = None
 
-  @classmethod
-  def _normalize(cls, settings):
-    """
-    Return a copy of `settings` (a dict) with any underscored keys replaced
-    with camelCased keys. Useful for passing Python-style dicts to Tokeninput,
-    which expects strange camelCase dicts.
-    """
+    def __init__(self, attrs=None, **kwargs):
+        super(TokenWidget, self).__init__(attrs)
+        self.settings = self._normalize(kwargs)
 
-    return dict([
-      (cls._camelcase(key), val)
-      for key, val in settings.items()
-    ])
+    @classmethod
+    def _normalize(cls, settings):
+        """
+        Return a copy of `settings` (a dict) with any underscored keys replaced
+        with camelCased keys. Useful for passing Python-style dicts to Tokeninput,
+        which expects strange camelCase dicts.
+        """
 
-  @staticmethod
-  def _camelcase(s):
-    return re.sub("_(.)", lambda m: m.group(1).capitalize(), s)
+        return dict([
+        (cls._camelcase(key), val)
+        for key, val in settings.items()
+        ])
 
-  @staticmethod
-  def _class_name(value):
-    return value.replace(" ", "-")
+    @staticmethod
+    def _camelcase(s):
+        return re.sub("_(.)", lambda m: m.group(1).capitalize(), s)
 
-  def render_value(self, value):
-      return unicode(value)
+    @staticmethod
+    def _class_name(value):
+        return value.replace(" ", "-")
 
-  def render(self, name, value, attrs=None):
-    flat_value = ",".join(map(unicode, value or []))
-    settings = copy.copy(self.settings)
+    def render_value(self, value):
+        return unicode(value)
 
-    if not self.search_url and self.search_view:
-        self.search_url = reverse(self.search_view)
-    attrs["data-search-url"] = self.search_url
+    def render(self, name, value, attrs=None):
+        flat_value = ",".join(map(unicode, value or []))
+        settings = copy.copy(self.settings)
 
-    attrs["class"] = self._class_name(
-      attrs.get("class"), "tokeninput")
+        if not self.search_url and self.search_view:
+            self.search_url = reverse(self.search_view)
+        attrs["data-search-url"] = self.search_url
 
-    if value is not None:
-      settings["prePopulate"] = [
-        {"id": pk, "name": self.render_value(self.choices.queryset.get(pk=pk))}
-        for pk in value
-      ]
+        attrs["class"] = self._class_name(
+            attrs.get("class"), "tokeninput")
 
-    attrs["data-settings"] = json.dumps(settings)
-    return super(TokenWidget, self).render(name, flat_value, attrs)
+        if value is not None:
+            settings["prePopulate"] = [
+                {"id": pk, "name": self.render_value(self.choices.queryset.get(pk=pk))}
+              for pk in value
+            ]
 
-  @staticmethod
-  def _class_name(class_name=None, extra=None):
-    return " ".join(filter(None, [class_name, extra]))
+        attrs["data-settings"] = json.dumps(settings)
+        return super(TokenWidget, self).render(name, flat_value, attrs)
 
-  def value_from_datadict(self, data, files, name):
-    values = data.get(name, "").split(",")
-    return self.clean_keys(values)
+    @staticmethod
+    def _class_name(class_name=None, extra=None):
+        return " ".join(filter(None, [class_name, extra]))
 
-  def clean_keys(self, values):
-    return [int(x) for x in values if x.strip().isdigit()]
+    def value_from_datadict(self, data, files, name):
+        values = data.get(name, "").split(",")
+        return self.clean_keys(values)
+
+    def clean_keys(self, values):
+        return [int(x) for x in values if x.strip().isdigit()]
